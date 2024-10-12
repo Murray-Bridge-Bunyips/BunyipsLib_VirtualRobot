b46b8cb211367caec00e0a69620813261df92958
Lucas Bubner <lkbubner@proton.me>
2024-10-12 20:13:04 +1030

    Port BunyipsLib changes

--- TeamCode/src/org/murraybridgebunyips/imposter/teleop/ImposterSwitchTest.java
+++ TeamCode/src/org/murraybridgebunyips/imposter/teleop/ImposterSwitchTest.java
@@ -3,24 +3,27 @@ package org.murraybridgebunyips.imposter.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.murraybridgebunyips.bunyipslib.BunyipsOpMode;
 import org.murraybridgebunyips.bunyipslib.Controls;
+import org.murraybridgebunyips.bunyipslib.ProfiledServo;
+import org.murraybridgebunyips.bunyipslib.external.TrapezoidProfile;
 import org.murraybridgebunyips.bunyipslib.subsystems.Switch;
 import org.murraybridgebunyips.imposter.components.ImposterConfig;
 
 @TeleOp
 public class ImposterSwitchTest extends BunyipsOpMode {
     private final ImposterConfig config = new ImposterConfig();
-    private Switch servo;
+
+    private ProfiledServo servo;
 
     @Override
     protected void onInit() {
         config.init();
-        servo = new Switch(config.back_servo);
+        servo = new ProfiledServo(config.back_servo);
+        servo.setConstraints(new TrapezoidProfile.Constraints(1, 0.8));
     }
 
     @Override
     protected void activeLoop() {
-        if (gamepad1.getDebounced(Controls.A)) servo.toggle();
-
-        servo.update();
+        servo.setPosition(gamepad1.a ? 0.5 : 0.3);
+        t.add("servo pos %", servo.getPosition());
     }
 }
