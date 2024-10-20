8b32cd482aa6d5aa0250b7b657dbaf01852977ff
Lucas Bubner <lkbubner@proton.me>
2024-10-20 18:09:21 +1030

    Port BunyipsLib rr1 changes

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/imposter/teleop/ImposterDrive.java
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/imposter/teleop/ImposterDrive.java
@@ -1,20 +1,19 @@
 package au.edu.sa.mbhs.studentrobotics.virtual.imposter.teleop;
 
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsOpMode;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.CommandBasedBunyipsOpMode;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.localization.MecanumLocalizer;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.drive.MecanumDrive;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.virtual.imposter.components.ImposterConfig;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class ImposterDrive extends BunyipsOpMode {
+public class ImposterDrive extends CommandBasedBunyipsOpMode {
     private final ImposterConfig config = new ImposterConfig();
     private MecanumDrive drive;
 
     @Override
-    protected void onInit() {
+    protected void onInitialise() {
         config.init();
         drive = new MecanumDrive(config.driveModel, config.motionProfile, config.mecanumGains, config.front_left_motor, config.back_left_motor, config.back_right_motor, config.front_right_motor, config.lazyImu, hardwareMap.voltageSensor);
         MecanumLocalizer l = (MecanumLocalizer) drive.getLocalizer();
@@ -23,8 +22,7 @@ public class ImposterDrive extends BunyipsOpMode {
     }
 
     @Override
-    protected void activeLoop() {
-        drive.setPower(Controls.makeRobotVel(gamepad1.lsx, gamepad1.lsy, gamepad1.rsx));
-        drive.update();
+    protected void assignCommands() {
+        
     }
 }
