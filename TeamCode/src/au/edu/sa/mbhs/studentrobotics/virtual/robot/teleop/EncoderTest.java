ef38eb4feb3a091b2812cf5f399fc24ea618c901
Lucas Bubner <lkbubner@proton.me>
2024-10-22 13:38:04 +1030

    Port BunyipsLib changes

---
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/teleop/EncoderTest.java
@@ -0,0 +1,29 @@
+package au.edu.sa.mbhs.studentrobotics.virtual.robot.teleop;
+
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsOpMode;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.Encoder;
+import au.edu.sa.mbhs.studentrobotics.virtual.robot.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class EncoderTest extends BunyipsOpMode {
+    private final Robot robot = new Robot();
+    private Encoder encoder;
+
+    @Override
+    protected void onInit() {
+        robot.init();
+        encoder = robot.hw.back_left_motor.getEncoder();
+    }
+
+    @Override
+    protected void activeLoop() {
+        t.add(encoder.getPosition());
+        robot.hw.back_left_motor.setPower(-gamepad1.lsy);
+        if (gamepad1.a)
+            encoder.setKnownPosition(3000);
+        if (gamepad1.b)
+            encoder.reset();
+    }
+}
+// giulio was here
