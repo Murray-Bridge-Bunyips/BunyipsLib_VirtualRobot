b8438788ba2b0cef3578e40fb6c3e41566353c3b
Lucas Bubner <lkbubner@proton.me>
2024-10-22 23:13:11 +1030

    Virtual robot

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/teleop/Drive.java
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/teleop/Drive.java
@@ -1,30 +1,21 @@
 package au.edu.sa.mbhs.studentrobotics.virtual.robot.teleop;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.CommandBasedBunyipsOpMode;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.drive.SimpleMecanumDrive;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.HolonomicDriveTask;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.virtual.robot.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import virtual_robot.config.Config;
 
 @TeleOp
 public class Drive extends CommandBasedBunyipsOpMode {
     private final Robot robot = new Robot();
-    private SimpleMecanumDrive drive;
 
     @Override
     protected void onInitialise() {
         robot.init();
-        drive = new SimpleMecanumDrive(robot.hw.front_left_motor, robot.hw.back_left_motor, robot.hw.back_right_motor, robot.hw.front_right_motor);
     }
 
     @Override
     protected void assignCommands() {
-    }
-
-    protected void periodic() {
-        drive.setPower(Controls.vel(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x));
-        drive.update();
+        robot.drive.setDefaultTask(new HolonomicDriveTask(gamepad1, robot.drive));
     }
 }
