cb950bda88fcf2dd521c1b2f05e6d8f06a166647
Lucas Bubner <lkbubner@proton.me>
2024-11-13 18:14:16 +1030

    Port BunyipsLib changes

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/bunyipslib/roadrunner/SymmetricPoseMap.kt
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/bunyipslib/roadrunner/SymmetricPoseMap.kt
@@ -8,11 +8,11 @@ import com.acmerobotics.roadrunner.PoseMap
  * Perform a reflection across the X-axis and mirror through the Y-axis. This is useful
  * for symmetric fields where a Y-axis reflection is also required, such as the INTO THE DEEP field.
  *
- * (x,y,r) -> (-x,-y,-r)
+ * (x,y,r) -> (-x,-y,r+PI)
  * @author Lucas Bubner, 2024
  * @since 6.0.0
  */
 class SymmetricPoseMap : PoseMap {
     override fun map(pose: Pose2dDual<Arclength>) =
-        Pose2dDual(-pose.position.x, -pose.position.y, pose.heading.inverse())
+        Pose2dDual(-pose.position.x, -pose.position.y, pose.heading + Math.PI)
 }
