6cc0b36a2e0839545da48b558fe91b81da3d62fa
Lucas Bubner <lkbubner@proton.me>
2025-02-06 21:39:18 +1030

    Encoder testings

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/teleop/EncoderTest.java
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/teleop/EncoderTest.java
@@ -1,32 +1,81 @@
 package au.edu.sa.mbhs.studentrobotics.virtual.robot.teleop;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsOpMode;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.Encoder;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.RobotConfig;
-import au.edu.sa.mbhs.studentrobotics.virtual.robot.Robot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.Mathf;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.control.pid.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Seconds;
 
 @TeleOp
 @RobotConfig.InhibitAutoInit
 public class EncoderTest extends BunyipsOpMode {
-    private final Robot robot = new Robot();
-    private Encoder encoder;
+    private BufferedWriter file;
+    private PIDFController pid;
+    
+    private double displacement = 0;
+    private double velocity = 0;
 
     @Override
     protected void onInit() {
-        robot.init();
-        encoder = robot.hw.back_left_motor.encoder;
+        try {
+            file = new BufferedWriter(new FileWriter("./data.csv"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        pid = new PIDFController(20, 0.0, 20, 0.0);
+        pid.setDerivativeSmoothingGain(0.8);
+        try {
+            file.write("time,s,v,a");
+            file.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        setLoopSpeed(Seconds.of(0.1));
     }
 
     @Override
     protected void activeLoop() {
-        t.add(encoder.getPosition());
-        robot.hw.back_left_motor.setPower(-gamepad1.lsy);
-        if (gamepad1.a)
-            encoder.setKnownPosition(3000);
-        if (gamepad1.b)
-            encoder.reset();
+        double target = 100;
+        double maxVelocity = 10;
+        double maxAcceleration = 5;
+        
+        double targetVelocity = pid.calculate(displacement, target);
+        double dV = Mathf.moveTowards(velocity, targetVelocity, maxAcceleration * timer.deltaTime().in(Seconds)) - velocity;
+        if (velocity >= maxVelocity && dV > 0)
+            dV = 0;
+        if (velocity <= -maxVelocity && dV < 0)
+            dV = 0;
+        double dVdT = dV / timer.deltaTime().in(Seconds);
+        velocity += dV;
+        velocity = Mathf.clamp(velocity, -maxVelocity, maxVelocity);
+        displacement += velocity * timer.deltaTime().in(Seconds);
+        telemetry.addData("s", displacement);
+        telemetry.addData("ds/dt", velocity);
+        telemetry.addData("d^2s/dt^2", dVdT);
+        double pidError = Math.abs(pid.getErrorDerivative());
+        telemetry.addData("pidError", pidError);
+        try {
+            file.write(String.format("%f,%f,%f,%f\n", timer.elapsedTime().in(Seconds), displacement, velocity, dVdT));
+            file.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    protected void onStop() {
+        try {
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 // giulio was here
