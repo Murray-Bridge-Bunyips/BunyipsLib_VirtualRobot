589274ff7d4ad33808276e9a7e110fe1e67990a8
Lucas Bubner <lkbubner@proton.me>
2025-04-01 23:11:51 +1030

    Port BunyipsLib changes

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/archived/TestManualUserSelection.java
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/virtual/robot/archived/TestManualUserSelection.java
@@ -2,6 +2,7 @@ package au.edu.sa.mbhs.studentrobotics.virtual.robot.archived;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsOpMode;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.executables.UserSelection;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Dbg;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Threads;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -10,11 +11,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.util.Arrays;
 
 @TeleOp
-@Disabled
+//@Disabled
 public class TestManualUserSelection extends BunyipsOpMode {
     @Override
     protected void onInit() {
-        var a = new UserSelection<>(Dbg::log, Arrays.asList("uno", new Object()), Arrays.asList("c", "a")).captionLayer(0, "CHOOSE SOMETHING");
+        var a = new UserSelection<>(Dbg::log, Arrays.asList("uno", new Object()), Arrays.asList("c", "a"))
+                .captionLayer(0, "CHOOSE SOMETHING")
+                .assignButton(0, 0, Controls.RIGHT_BUMPER);
 //        a.disableChaining();
         Threads.start("a", a);
     }
