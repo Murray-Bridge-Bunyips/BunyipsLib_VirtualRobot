69b6ac0fc7c98b5855a1ad591faf39eeaa5b4d52
Lucas Bubner <lkbubner@proton.me>
2025-11-18 23:36:55 +1030

    Port BunyipsLib changes

--- TeamCode/src/au/edu/sa/mbhs/studentrobotics/bunyipslib/BuildConfig.java
+++ TeamCode/src/au/edu/sa/mbhs/studentrobotics/bunyipslib/BuildConfig.java
@@ -9,7 +9,7 @@ public class BuildConfig {
     public static final String BUILD_TIME = "virtual-robot";
     public static final String GIT_COMMIT = "master";
     public static final String ID = "VIRTUAL_BUNYIPSFTC";
-    public static final String SEMVER = "8.0.0";
+    public static final String SEMVER = "8.0.1";
     public static final String SDK_VER = "N/A";
     public static final String LIBRARY_PACKAGE_NAME = "au.edu.sa.mbhs.studentrobotics.bunyipslib";
 }
